90ed227a358d2bcb3e011aa104fff36a4cdf2606
Thomas Harmeyer <thomasmharmeyer@gmail.com>
2021-01-10 17:22:24 -0500

    yuup

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ app.get("/", (req, res) => {
     res.send("hello world");
 });
 
-const port = process.env.port || 3000;
+const port = process.env.port || 8081;
 app.listen(port, () => {
     console.log("hit");
 });
